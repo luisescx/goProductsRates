55ee2899bcbc211df59bfd48876d17ba244d353b
luisescx <luis.esculapio@live.com>
2023-02-25 19:35:47 -0300

    feat: add consumer and producer

---
+++ cmd/consumer/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"time"
+
+	"github.com/luisescx/goProductsRates/internal/order/infra/database"
+	"github.com/luisescx/goProductsRates/internal/order/usecase"
+	"github.com/luisescx/goProductsRates/pkg/rabbitmq"
+	_ "github.com/mattn/go-sqlite3"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./orders.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	repository := database.NewOrderRepository(db)
+	uc := usecase.CalculateFinalPriceUseCase{OrderRepository: repository}
+
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	out := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, out)
+
+	for msg := range out {
+		var inputDTO usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &inputDTO)
+		if err != nil {
+			panic(err)
+		}
+		outputDTO, err := uc.Execute(inputDTO)
+		if err != nil {
+			panic(err)
+		}
+		msg.Ack(false)
+		println(outputDTO)
+		time.Sleep(300 * time.Millisecond)
+	}
+}
